2ab98ba5e2f7fc1419b140616231657d8f183f61
Pieter Van Parys <pieter.vanparys@gmail.com>
2012-07-04 08:38:50 +0200

    Changed Customer Sample

--- MData.SandBox/Model/ICustomer.cs
+++ MData.SandBox/Model/ICustomer.cs
@@ -1,10 +1,20 @@
+using System;
 using MData.Attributes;
+using MData.Core.Base;
 
 namespace MData.SandBox
 {
     [MData("Customer")]
     public interface ICustomer : IId, ICustomerMethod
     {
-        string Data { get; }
+        int Id { get; set; }
+        string Name { get; set; }
+        string Notes { get; set; }
+        bool IsActive { get; set; }
+        DateTime CreatedOn { get; set; }
+        DateTime ModifiedOn { get; set; }
+
+        void Active();
+        void DeActivate();
     }
 }
